21a67676a15160dd9c33b8f04adfae20360f6dd5
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2022-10-13 17:22:58 +0200

    AddProject

---
+++ RefferenceKeyword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RefferenceKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            AddNumber(number);
+            Console.WriteLine(number);
+
+            AddNumber(ref number);
+            Console.WriteLine(number);
+
+
+        }
+        public static void AddNumber(int number)
+        {
+            number++;
+        }
+        public static void AddNumber(ref int number)
+        {
+            number++;
+        }
+    }
+}
